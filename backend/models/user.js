0109fad36bc45d56d5b54e2aebf6aeb203d796bf
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-12-03 21:57:31 +0300

    -User Collection

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose"); //mongoose çağırdık
+const { stringify } = require("uuid");
+
+const userSchema= new mongoose.Schema({ //Schema oluşturuyoruz.
+    _id: String, 
+    name:{
+        type: String,
+        required: true 
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: Boolean,
+    createdDate: Date
+})
+
+const User = mongoose.model("User",userSchema); //userı tanımladık
+
+module.exports = User //userı paylaşıma açtık
