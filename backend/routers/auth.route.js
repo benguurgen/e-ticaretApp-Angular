8836bdd2741fc7cf085bb252b9e70f2694c86999
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-12-04 17:01:43 +0300

    -Login Api

--- backend/routers/auth.route.js
+++ backend/routers/auth.route.js
@@ -34,4 +34,25 @@ router.post("/register",async(req,res)=>{  //post oluşturuyoruz.
     }    
 })
 
+router.post("/login", async(req,res)=>{
+    try{
+        const {email, password} = req.body;
+
+        let user = await User.findOne({email: email});
+        if(user==null){
+            res.status(403).json({message: "Kullanıcı bulunamadı!"})
+        }else{
+            if(user.password != password){
+                res.status(403).json({message: "Şifre yanlış!"})
+            }else{
+                const token = jwt.sign({},secretKey,options);
+                let model = {token: token, user: user};
+                res.json(model);
+            }
+        }
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+})
+
 module.exports = router; //routerı paylaştık
