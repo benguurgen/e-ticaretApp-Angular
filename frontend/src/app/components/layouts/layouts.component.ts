a6225ea4b9b48adbfc100652cbe3cec00712fc2a
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-12-08 19:33:03 +0300

    -Kategori ve layout html yapısı

--- frontend/src/app/components/layouts/layouts.component.ts
+++ frontend/src/app/components/layouts/layouts.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
   imports: [
-    SharedModule
+    SharedModule,
+    NavbarComponent
   ],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
