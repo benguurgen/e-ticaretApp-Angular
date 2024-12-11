bfb98bbad1ce651c4de5538a58915b66e8c7ae54
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-12-11 19:40:30 +0300

    -Response Service

--- backend/routers/category.router.js
+++ backend/routers/category.router.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../models/category");
 const { v4: uuidv4 } = require("uuid")
+const response = require("../services/response.service")
 
 router.post("/add", async (req, res) => {
-    try {
+    response(res, async () => {
         const { name } = req.body;
 
         const checkName = await Category.findOne({ name: name });
@@ -16,29 +17,22 @@ router.post("/add", async (req, res) => {
                 _id: uuidv4(),
                 name: name
             });
-
             await category.save();
             res.json({ message: "Kategori kaydı başarıyla tamamlandı!" })
-
         }
-
-    } catch (error) {
-        res.stats(500).json({ message: error.message })
-    }
+    })
 });
 
 router.post("/removeById", async (req, res) => {
-    try {
+    response(res, async () => {
         const { _id } = req.body;
-        await Category.findByIdAndDelete({_id});
+        await Category.findByIdAndDelete({ _id });
         res.json({ message: "Kategori kaydı başarıyla silindi!!" })
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
+    });
 });
 
 router.post("/update", async (req, res) => {
-    try {
+    response(res, async () => {
         const { _id, name } = req.body;
         const category = await Category.findOne({ _id: _id });
         if (category.name != name) {
@@ -51,18 +45,14 @@ router.post("/update", async (req, res) => {
                 res.json({ message: "Kategori kaydı başarıyla güncellendi!!" })
             }
         }
-    } catch (error) {
-        res.stats(500).json({ message: error.message })
-    }
-})
+    });
+});
 
 router.get("/", async (req, res) => {
-    try {
+    response(res, async () => {
         const categories = await Category.find().sort({ name: 1 });
         res.json(categories)
-    } catch (error) {
-        res.stats(500).json({ message: error.message })
-    }
+    });
 });
 
 module.exports = router;
