e05178667bd8e2c3c57cb4c3d7941bbd5e188ab5
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-12-11 20:07:03 +0300

    -Product id ye göre getirme metodu

--- backend/routers/product.router.js
+++ backend/routers/product.router.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/product");
-const {v4: uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const upload = require("../services/file.service");
 const response = require("../services/response.service")
 
 //Ürün Ekleme
-router.post("/add",upload.array("images"),async(req,res)=>{
-    response(res, async ()=> {
-        const {name, stock, price, categories} = req.body;
+router.post("/add", upload.array("images"), async (req, res) => {
+    response(res, async () => {
+        const { name, stock, price, categories } = req.body;
 
         const productId = uuidv4();
         let product = new Product({
@@ -22,63 +22,71 @@ router.post("/add",upload.array("images"),async(req,res)=>{
             imageUrls: req.files,
             createdDate: new Date()
         });
-        
+
         await product.save();
 
-        res.json({message: "Ürün kaydı başarıyla tamamlandı.."});
-        
+        res.json({ message: "Ürün kaydı başarıyla tamamlandı.." });
+
     });
 
 })
 //Ürün Silme
-router.post("/removeById",async (req,res)=>{
-    response(res,async()=>{
-        const {_id}= req.body;
+router.post("/removeById", async (req, res) => {
+    response(res, async () => {
+        const { _id } = req.body;
         const product = await Product.findById(_id);
-        for(const image of product.imageUrls){
-            fs.unlink(image.path, ()=>{});
+        for (const image of product.imageUrls) {
+            fs.unlink(image.path, () => { });
         }
         await Product.findByIdAndDelete(_id);
-        res.json({message: "Ürün kaydı başarıyla silindi!"})
+        res.json({ message: "Ürün kaydı başarıyla silindi!" })
     })
-} )
+})
 //Ürün Listesi Getir
-router.post("/", async (req, res)=>{
-    response(res,async()=>{
-        const {pageNumber,pageSize,search} =req.body;
+router.post("/", async (req, res) => {
+    response(res, async () => {
+        const { pageNumber, pageSize, search } = req.body;
 
         let productCount = await Product.find({
-            $or:[
+            $or: [
                 {
-                    name: {$regex:search, $options:'i'}
+                    name: { $regex: search, $options: 'i' }
                 }
             ]
         }).count();
 
         let products = await Product
-        .find({
-            $or:[
-                {
-                    name: {$regex:search, $options:'i'}
-                }
-            ]
-        })
-        .sort({name:1})
-        .populate("categories")
-        .skip((pageNumber - 1)*pageSize)
-        .limit(pageSize);
+            .find({
+                $or: [
+                    {
+                        name: { $regex: search, $options: 'i' }
+                    }
+                ]
+            })
+            .sort({ name: 1 })
+            .populate("categories")
+            .skip((pageNumber - 1) * pageSize)
+            .limit(pageSize);
 
-        let totalPageCount = Math.ceil(productCount/pageSize);
+        let totalPageCount = Math.ceil(productCount / pageSize);
         let model = {
             datas: products,
             pageNumber: pageNumber,
             pageSize: pageSize,
             totalPageCount: totalPageCount,
-            isFirstPage: pageNumber ==1? true: false,
-            isLastPage: totalPageCount == pageNumber ? true:false,
+            isFirstPage: pageNumber == 1 ? true : false,
+            isLastPage: totalPageCount == pageNumber ? true : false,
 
         };
 
         res.json(model);
     })
 })
+//Ürünü Idye göre getir
+router.post("/getById", async (req, res) => {
+    response(res, async () => {
+        const { _id } = req.body;
+        let product = await Product.findById(_id);
+        res.json(product);
+    });
+});
