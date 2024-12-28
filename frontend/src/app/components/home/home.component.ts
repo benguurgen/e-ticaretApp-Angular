157c224e6a90258ef1012f0dcedcddbf068e6a5f
Bengü Ürgen Demir <benguurgen@gmail.com>
2024-12-28 12:00:52 +0300

    -Anasayfa html tasarımı

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { CategoryModel } from '../categories/models/category.model';
+import { CategoryService } from '../categories/services/category.service';
+import { RequestModel } from '../../common/models/request.model';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,27 @@ import { SharedModule } from '../../common/shared/shared.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  categories: CategoryModel[] = [];
+  request: RequestModel = new RequestModel();
 
+  constructor(
+    private _category: CategoryService
+  ) { }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getAll(){
+    
+  }
+
+  getCategories() {
+    this._category.getAll(res => this.categories = res)
+  }
+
+  changeCategory(categoryName: string){
+    this.request.categoryName = categoryName
+  }
 }
+
